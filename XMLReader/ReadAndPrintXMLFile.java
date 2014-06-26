91195eee2066b832ec697086cf493de25a727d49
visustock-dev <dev@visustock.fr>
2014-06-26 12:07:11 -0500

    Latest build with more error handling and comments.

--- XMLReader/ReadAndPrintXMLFile.java
+++ XMLReader/ReadAndPrintXMLFile.java
@@ -12,76 +12,58 @@ import org.xml.sax.SAXParseException;
 
 public class ReadAndPrintXMLFile{
 public static void main (String[] args){
-	
-	for (String s: args) {
+
+	for (String s: args) {//File will run for each string entered in the cmd separated by white space
         System.out.println(s);
         //String i = "xml$";
-        String [] splitString = s.split("xml\\$");  //s.split("xml//$");
-        int totalLength = splitString.length;
-        System.out.println(splitString[0]);
-        System.out.println(totalLength);
-        if(totalLength != 1){
-        	System.out.println("The file is not type .xml");
-        	System.exit(1);
+        //String [] splitString = s.split("xml\\$");  //s.split("xml//$");
+        
+        
+        
+        if(Checker(s) == 0){
+        int fileValid = -1;
+        
+        if(s.matches(".*\\.xml.*$")){
+        	System.out.println("file is type XML MATCHES SUCCESS");
+        	fileValid = 0;
         }
         else{
-        	System.out.println("File is type .xml");
+        	System.out.println("MATCHES DID NOT DETECT XML");
+        	fileValid = 1;
         }
         
-        //String strWithNumber = "This string has a 1 number";
-        /*if(s.matches(".*\\d.*")){
-            System.out.println("ERROR, the specified");
-        } else{
-            System.out.println("ERROR, ID must contain a number.");
-        }
-
-        String strWithoutNumber = "This string does not have a number";
-        if(strWithoutNumber.matches(".*\\d.*")){
-            System.out.println("'"+strWithoutNumber+"' contains digit");
-        } else{
-            System.out.println("'"+strWithoutNumber+"' does not contain a digit");
-        }*/
-        //if(s.matches("petsErrorTest.xml")){
-        	//System.out.println("TRUE");
-        //}
-        //if(i.test(s)){
-        	
-        //}
-        
-        
-    /**
-     * @Checker(s);
-     * This method checks to make sure that the file specified by the args exists before executing the rest of the program
-     */
-        
-    Checker(s);
+    if(fileValid == 0){
     System.out.println("------------");
     
-    /**
-     * @getDoc(s);
-     * This method opens the specified file and creates a document variable to be used by the other methods
-     */
+    
     
     Document doc = getDoc(s);
     
-    /**
-     * @getPets(doc);
-     * This method lists the people in the file
-     */
     
-    getPeople(doc);
+    
+    String[][] People = getPeople(doc);
+    
     System.out.println("------------");
     
-    /**
-     * @getPets(doc, "<petType>");
-     * This method lists the pets and their owners from the file
-     */
     
-    getPets(doc, "dog");
+    
+    getPets(doc, "dog", People);
     System.out.println("------------");
-    getPets(doc, "cat");
+    getPets(doc, "cat", People);
+    }
+    
+	}
+        System.out.println("------------");
 	}
     }//end of main
+
+/**
+ * getDoc(s)
+ * This method opens the specified file and creates a document variable to be used by the other methods
+ * @Param String
+ */
+
+
     
     public static Document getDoc(String s){//Creates a document variable from the file input
     	Document doc = null;
@@ -96,7 +78,7 @@ public static void main (String[] args){
 	            doc.getDocumentElement ().normalize ();
 	            System.out.println ("Root element of the doc is " + 
 	                 doc.getDocumentElement().getNodeName());
-	            
+
 	    }catch (SAXParseException err) {
 	        System.out.println ("** Parsing error" + ", line " 
 	             + err.getLineNumber () + ", uri " + err.getSystemId ());
@@ -114,18 +96,30 @@ public static void main (String[] args){
 } //getDoc()
    
     
-    public static void getPeople(Document doc){//Displays the people in the XML file
+    /**
+     * getPeople(doc);
+     * This method lists the people in the file
+     * @Param Document
+     */
+    
+    
+    public static String[][] getPeople(Document doc){//Displays the people in the XML file
     	try {
 
             NodeList listOfPersons = doc.getElementsByTagName("person");
             int totalPersons = listOfPersons.getLength();
             System.out.println("Total no of people : " + totalPersons);
             
-            String[] peopleFNameArray;//----Declares an array of the owners First Names
+            String[][] People = new String[totalPersons][3];
+            
+            int valid = 0;
+            
+            
+            /*String[] peopleFNameArray;//----Declares an array of the owners First Names
             peopleFNameArray = new String[listOfPersons.getLength()];
             
             String[] peopleLNameArray;//----Declares an array of the owners Last Names
-            peopleLNameArray = new String[listOfPersons.getLength()];
+            peopleLNameArray = new String[listOfPersons.getLength()];*/
             
             String[] peopleIDArray;//----Declares an array of the owners First Names
             peopleIDArray = new String[listOfPersons.getLength()];
@@ -147,9 +141,17 @@ public static void main (String[] args){
                     System.out.println("First Name : " + 
                            ((Node)textFNList.item(0)).getNodeValue().trim());
                     
-                    RegExDigitChecker(((Node)textFNList.item(0)).getNodeValue().trim() ,  0);
+                    if(RegExDigitChecker(((Node)textFNList.item(0)).getNodeValue().trim() ,  0)==0){
+                    	//nameValid = 1;
+                    	People[s][1] = ((String)(textFNList.item(0)).getNodeValue().trim());//------------------This puts the persons first name into the second index location of the array for that person in the multidimensional array
+                    	
+                    }
+                    else{
+                    	People[s][1] = "ERROR";
+                    }
                     
-                    peopleFNameArray[s] = ((String)(textFNList.item(0)).getNodeValue().trim());
+                    
+                    //People[s][1] = ((String)(textFNList.item(0)).getNodeValue().trim());//------------------This puts the persons first name into the second index location of the array for that person in the multidimensional array
                    // System.out.println( "Name in string at index : " + s + " : " + peopleFNameArray[s]);
 
                     //-------
@@ -160,9 +162,14 @@ public static void main (String[] args){
                     System.out.println("Last Name : " + 
                            ((Node)textLNList.item(0)).getNodeValue().trim());
                     
-                    RegExDigitChecker(((Node)textLNList.item(0)).getNodeValue().trim() ,  0);
+                    if(RegExDigitChecker(((Node)textLNList.item(0)).getNodeValue().trim() ,  0) == 0){
+                    	People[s][2] = ((String)(textLNList.item(0)).getNodeValue().trim());//-----------------------Puts in the last name into the array location 
+                    }
+                    else{
+                    	People[s][2] = "ERROR";
+                    }
                     
-                    peopleLNameArray[s] = ((String)(textLNList.item(0)).getNodeValue().trim());
+                    //People[s][2] = ((String)(textLNList.item(0)).getNodeValue().trim());//-----------------------Puts in the last name into the array location 
                   //  System.out.println( "Name in string at index : " + s + " : " + peopleLNameArray[s]);
                     
                     //----
@@ -174,16 +181,19 @@ public static void main (String[] args){
                     NodeList textIdList = idElement.getChildNodes();
                     System.out.println("Id : " + 
                     ((Node)textIdList.item(0)).getNodeValue().trim());
-                    
+                    valid = 0;
                     try{
                         double d= Double.valueOf(((Node)textIdList.item(0)).getNodeValue().trim());
                         if (d==(int)d&&d>0){
                         	//ID is an integer above 0
+                        	valid = 0;
                         }else{//ID is not an integer above 0
                         	System.out.println("ERROR, all IDs must be integer values");
                             System.out.println("Please change this id: "+ d);
+                            valid = 1;
                         }
                     }catch(Exception e){
+                    	valid = 1;
                         System.out.println("ERROR, IDs must be integers only");
                     }
                     //if (s == 0){
@@ -194,7 +204,8 @@ public static void main (String[] args){
                     //}
                     if(s == 0){
                     	for(int z = 0; z<totalPersons; z++){
-                    		peopleIDArray[z] = "BLANK";
+                    		peopleIDArray[z] = "NoID";
+                    		People[z][0] = "NoId";
                     		//peopleIDArray[z] = ((Node)textIdList.item(0)).getNodeValue().trim();
                     		//if(idChecker(peopleIDArray, ((Node)textIdList.item(0)).getNodeValue().trim(), z )==0){
                     		//	System.out.println("Error, duplicate IDs detected");
@@ -206,9 +217,14 @@ public static void main (String[] args){
                     if(s!= 0){
                     if(idChecker(peopleIDArray, ((Node)textIdList.item(0)).getNodeValue().trim(), s )==0){
             			System.out.println("Error, duplicate IDs detected");
+            			valid = 1;
                     }
                     }
-                    peopleIDArray[s] = ((Node)textIdList.item(0)).getNodeValue().trim();
+                    if(valid==0){
+                    	peopleIDArray[s] = ((Node)textIdList.item(0)).getNodeValue().trim();
+                    }
+                    
+                    
                    /* System.out.println(idChecker(peopleIDArray, ((Node)textIdList.item(0)).getNodeValue().trim(), s ) + "   " + s);
                     //if(s != 0){
 	                    	
@@ -230,7 +246,7 @@ public static void main (String[] args){
 	                    	//}
 	                    	
 	                    }*/
-	                    
+
                     	
                    // }
                     
@@ -244,11 +260,11 @@ public static void main (String[] args){
                     
                     
                    
-	
+
 	                    //----
 	                    NodeList ageList = firstPersonElement.getElementsByTagName("age");
 	                    Element ageElement = (Element)ageList.item(0);
-	
+
 	                    NodeList textAgeList = ageElement.getChildNodes();
 	                    System.out.println("Age : " + 
 	                           ((Node)textAgeList.item(0)).getNodeValue().trim());
@@ -261,6 +277,16 @@ public static void main (String[] args){
 
 
             }//end of for loop with s var
+            
+            for(int i = 0; i<totalPersons; i++){
+            	People[i][0] = peopleIDArray[i]; 
+            }
+            
+            for(int b = 0; b<totalPersons; b++){
+            	//People[i][0] = peopleIDArray[b]; 
+            	System.out.println(People[b][0]+"  " + People[b][1] + " " + People[b][2]);
+            }
+            return People;
             
           
             
@@ -295,10 +321,22 @@ public static void main (String[] args){
         t.printStackTrace ();
         }
         //System.exit (0);
+		return null;
     	
     }
     
-    public static void getPets(Document doc, String petType){//Displays dogs in the XML file
+    
+    /**
+     * getPets(doc, "<petType>");
+     * This method lists the pets and their owners from the file
+     * @Param Document
+     * @Param String
+     * @Param String[][]
+     */
+    
+    
+    public static void getPets(Document doc, String petType, String[][] People){//Displays dogs in the XML file
+    	
     	try {
     		
     		
@@ -374,6 +412,8 @@ public static void main (String[] args){
                         System.out.println("Name : " + 
                                ((Node)textLNList.item(0)).getNodeValue().trim());
                         
+                        
+                        
                         RegExDigitChecker(((Node)textLNList.item(0)).getNodeValue().trim() ,  0);
 
                         //----
@@ -407,7 +447,7 @@ public static void main (String[] args){
                         //System.out.println(x);
                         if(test==0){
                         	int x = Integer.parseInt(textOwnerList.item(0).getNodeValue());
-                        	owner(x, doc);
+                        	owner(x, doc, People);
                         	
                         }
                         else{
@@ -466,27 +506,31 @@ public static void main (String[] args){
      * This method prints the names of the owner of a pet based on the owner id from the person and the pet
      */
     
-    public static void owner(int b, Document doc){//Displays who owns the dog or cat, using a person from the people element
-    	int s = b-1;
+    public static void owner(int b, Document doc, String[][] People){//Displays who owns the dog or cat, using a person from the people element
+    	//int s = b-1;
+    	int p = -1;
     	
-    	try {
+    	//try {
+    	
+    	NodeList listOfPersons = doc.getElementsByTagName("person");
+        int totalPersons = listOfPersons.getLength();
 
-            NodeList listOfPersons = doc.getElementsByTagName("person");
+            //NodeList listOfPersons = doc.getElementsByTagName("person");
             //int totalPersons = listOfPersons.getLength();
             //System.out.println("Total no of people : " + totalPersons);
             
-            String[] peopleFNameArray;//----Declares an array of the owners First Names
-            peopleFNameArray = new String[listOfPersons.getLength()];
+            //String[] peopleFNameArray;//----Declares an array of the owners First Names
+            //peopleFNameArray = new String[listOfPersons.getLength()];
             
-            String[] peopleLNameArray;//----Declares an array of the owners Last Names
-            peopleLNameArray = new String[listOfPersons.getLength()];
+            //String[] peopleLNameArray;//----Declares an array of the owners Last Names
+            //peopleLNameArray = new String[listOfPersons.getLength()];
             //peopleArray = new String[10];
             
 
             
 
 
-                Node firstPersonNode = listOfPersons.item(s);
+                /*Node firstPersonNode = listOfPersons.item(s);
                 if(firstPersonNode.getNodeType() == Node.ELEMENT_NODE){
 
 
@@ -530,19 +574,73 @@ public static void main (String[] args){
                    // System.out.println("Age : " + 
                      //      ((Node)textAgeList.item(0)).getNodeValue().trim());
 
-                    //------
-                    System.out.println( "Owner : " + peopleFNameArray[s] + " " + peopleLNameArray[s]);
+                    //------*/
+        int owner = -1;
+        int success = 0;
+        /*for(int a = 0; a<totalPersons; a++){
+        	//People[i][0] = peopleIDArray[b]; 
+        	System.out.println(People[a][0]+"  " + People[a][1] + " " + People[a][2]);
+        }*/
+        //return People;
+        //System.out.println(String.valueOf(b));
+        //String number = String.valueOf(b);
+        
+        //System.out.println(number);
+        
+        //int y = -1;
+        //Integer.parseInt(People[i][0]);
+        
+        //number = number + 1;
+        //System.out.println(number);
+        
+        int ID = -1;
+    	for(int i = 0; i<totalPersons; i++){
+    		if(People[i][0] != "NoID"){
+    			 ID = Integer.parseInt(People[i][0]);
+    		}
+    		else{
+    			 ID = -1;
+    		}
+    		
+    		if(ID == b){
+    		//if(People[i][0] == number){
+    			
+    			owner = i;
+    			i = (totalPersons);
+    			i++;
+    			success = 1;
+    			
+    		}	
+    		//System.out.println("ID:    " + People[i][0] );
+    	}
+    	
+    	
+    	
+    	
+    	
+    	
+    	if(success == 1){
+    		System.out.println("Owner: " + People[owner][1] + " " + People[owner][2]);
+    	}
+    	else{
+    		System.out.println("No owner found with ID: " + b);
+    	}
+            		
+            		
+            		
+            		
+                    //System.out.println( "Owner : " + peopleFNameArray[s] + " " + peopleLNameArray[s]);
 
 
-                }//end of if clause
+                //}//end of if clause
 
 
             
 
 
-        }catch (Throwable t) {
-        t.printStackTrace ();
-        }
+        //}catch (Throwable t) {
+        //t.printStackTrace ();
+        //}
         //System.exit (0);
     	
     	
@@ -644,20 +742,29 @@ public static void main (String[] args){
     	
     	
     }
-    public static void Checker(String s) {
+    
+    /**
+     * Checker(s)
+     * This method checks to make sure that the file specified by the args exists before executing the rest of the program
+     * @Param String
+     */
+    
+    public static int Checker(String s) {
     	 
   	  File f = new File(s);
    
   	  if(f.exists()){
   		  System.out.println("File existed");
+  		  return 0;
   	  }else{
   		  System.out.println("File not found!");
-  		System.exit(1);
+  		//System.exit(1);
+  		  return 1;
   	  }
    
     }
     
-   public static void RegExDigitChecker(String x, int type){
+   public static int RegExDigitChecker(String x, int type){
 	   if(type == 1){//This type will be used to check the ID's 
 		   //String strWithNumber = "This string has a 1 number";
 		   //if(x.matches(".*\\w.*")){//String contains a letter
@@ -665,27 +772,31 @@ public static void main (String[] args){
 		  // }
 		   /*else*/ if(x.matches(".*\\d.*")){//String does not contain a letter and contains a number
 		       System.out.println("ID is valid");
+		       return 0;
 		   }
 		   else{//ID does not contain a number or a letter
 			   System.out.println("ERROR---------------------------");
+			   return 1;
 		   }
 	   }
 		   else if(type == 0){//This type will be used to check names
 			   //String strWithoutNumber = "This string does not have a number";
 			   if(x.matches(".*\\d.*")){//Contains a number
 			       System.out.println("ERROR, names may not contain numbers");
+			       return 1;
 			   } else{
 			       System.out.println("Name is valid");
+			       return 0;
 			   }
 		   }
-		   
-		   
-		   
-		   
-		   
-		   
-		   
-	   
+
+
+
+
+
+
+
+
 		   /*//if(x.matches(".*\\D.*")){//ID contains a non digit
 		   if(x.matches(".*[a-zA-Z]*")){
 	           //System.out.println("ERROR, the specified");
@@ -708,6 +819,7 @@ public static void main (String[] args){
 	   }
 	   
 	   return 0;*/
+	   return 0;
    }
 
    
